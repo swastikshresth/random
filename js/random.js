270f247278c8774d7edcb6f5c230221f35c9a900
unknown <sanusinghrajput95@gmail.com>
2021-01-20 17:44:29 +0530

    validation with console

--- js/random.js
+++ js/random.js
@@ -3,14 +3,18 @@ function random() {
     var state = document.getElementById("stateId").value;
     var city = document.getElementById("cityId").value;
     if (country.length <= 0) {
-        alert("this field this required");
+        alert("this field is required");
     }
     if (state.length <= 0) {
-        alert("this field this required");
+        alert("this field is required");
     }
     if (city.length <= 0) {
-        alert("this field this required");
+        alert("this field is required");
+
     } else {
         alert("you are successfully redirected");
+        console.log(country);
+        console.log(state);
+        console.log(city);
     }
 }
