ab86a2b8ba8488d91754205078bef20d2b5fb61b
unknown <sanusinghrajput95@gmail.com>
2021-01-20 18:26:07 +0530

    tips

--- js/random.js
+++ js/random.js
@@ -2,19 +2,21 @@ function random() {
     var country = document.getElementById("countryId").value;
     var state = document.getElementById("stateId").value;
     var city = document.getElementById("cityId").value;
+
     if (country.length <= 0) {
         alert("country fields are required");
     }
+
     if (state.length <= 0) {
         alert("state field are required");
     }
+
     if (city.length <= 0) {
         alert("city field are required");
 
     } else {
         alert("you are successfully redirected");
-        console.log(country);
-        console.log(state);
-        console.log(city);
+        console.log("country->" + country + " " + "state->" + state + "" + " " + "city-> " + city);
+
     }
 }
